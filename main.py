ebf2bf13471329184631f2c981b42282ca0c870a
Na3aga <na3aga@gmail.com>
2018-12-25 01:52:36 +0200

    2 graphics

---
+++ main.py
@@ -0,0 +1,142 @@
+import plotly.offline as pl
+from plotly import tools
+import plotly.graph_objs as go
+from model import get_dataset
+from collections import Counter
+
+dataset = get_dataset(220000)
+income_groups = set(dataset.keys())
+
+
+
+inc_and_regions = {'$0 to <$10,000':{},'$10,000-<$20,000':{},'$20,000-<$30,000':{},'$30,000-<$40,000':{},'$40,000-<$50,000':{},'$50,000+':{}}
+
+
+for i in list(dataset.keys()):
+    '''
+    fill income groups dictionary with values like...
+    {'Economic Development Region' : int(Number of Households)}
+    '''
+    counties = Counter()
+    for j in list(dataset[i].keys()):
+        for m in list(dataset[i][j].keys()):
+            counties[j] +=len(dataset[i][j][m])
+    inc_and_regions[i] = dict((k[0],k[1]) for k in counties.most_common(3))
+
+
+
+# print(inc_and_regions)
+
+
+x=list(inc_and_regions.keys())
+
+
+trace0 = go.Bar(
+    name='First by number of households',
+    x=x,
+    y=[list(i.values())[0] for i in list(inc_and_regions.values())],
+    text=[list(i.keys())[0] for i in list(inc_and_regions.values())],
+    textposition='auto',
+    marker=dict(
+        color='rgb(49,130,189)'
+    )
+)
+trace1 = go.Bar(
+    name='Second by number of households',
+    x=x,
+    y=[list(i.values())[1] for i in list(inc_and_regions.values())],
+    text=[list(i.keys())[1] for i in list(inc_and_regions.values())],
+    
+    textposition='auto',
+    marker=dict(
+        color='rgb(204,204,204)',
+    )
+    
+)
+trace2 = go.Bar(
+    name='Thirth by number of households',
+    x=x,
+    y=[list(i.values())[2] for i in list(inc_and_regions.values())],
+    text=[list(i.keys())[2] for i in list(inc_and_regions.values())],
+    textposition='auto',
+    marker=dict(
+        color='rgb(158, 64, 147)',
+    )
+)
+
+
+data = [trace0, trace1,trace2]
+
+
+layout = go.Layout(title='number of households in cities by income groups',
+                    titlefont={'size': 32, 'color': 'blue'}
+                  )
+
+fig = go.Figure(data=data, layout=layout)
+
+pl.plot(fig, filename='reg_incgroups_number.html')
+
+
+
+# PLOT NUMBER 2
+
+
+inc_and_races = {'$0 to <$10,000':{},'$10,000-<$20,000':{},'$20,000-<$30,000':{},'$30,000-<$40,000':{},'$40,000-<$50,000':{},'$50,000+':{}}
+
+for a in list(dataset.keys()):
+    '''
+    fill income groups dictionary with values like...
+    {'Race/Ethnicity' : int(number of households)}
+    '''
+    races = Counter()
+    for b in list(dataset[a].keys()):
+        for c in list(dataset[a][b].keys()):
+            for d in dataset[a][b][c]:
+                races[d['Race']] +=1
+    inc_and_races[a] = dict((k[0],k[1]) for k in races.most_common(10))
+
+
+
+def create_pie(dictionary, name='',domain = False,hoverinfo="label+percent+name",hole = .4):
+    result = dict()
+    result["type"] = "pie"
+    result["name"] = str(name)
+    result["hoverinfo"] = hoverinfo
+    result["hole"] = hole
+    if domain != False:
+        result["domain"] = domain
+    result["values"] = list(dictionary.values())
+    result["labels"] = list(dictionary.keys())
+
+    return result
+
+
+def create_domains(dictionary):
+    result = dict()
+    number = len(dictionary)
+    size = round(1.0 / number, 3) - 0.04
+    step = size  + 0.04 
+    x1,x2 = 0.0,size
+    for i in list(dictionary.keys()):
+        result[i] = {'x':[x1,x2]}
+        x1 += step
+        x2 += step 
+
+    return result
+    
+domains = create_domains(inc_and_races)
+
+data = [create_pie(inc_and_races[d],d,domain = domains[d] ,hole=0.2 ) for d in list(inc_and_races.keys())]
+
+fig = {
+    "data": data,
+    "layout": {
+        "title":"Race/Ethnicity percantage in income groups",
+        "titlefont":{
+            "size": 32,
+            "color": "green"
+        }
+    }
+}
+
+pl.plot(fig, filename = "racesincomes.html")
--- model.py
+++ model.py
@@ -5,9 +5,6 @@
 # Time in Home,Education Level,Head of Household Age,Household Weight
 import pandas as pd
 import csv
-import plotly.offline as pl
-import plotly.graph_objs as go
-from functools import reduce
 
 def get_dataset(stop=100):
     dataset = dict() 
@@ -125,80 +122,4 @@ def get_dataset(stop=100):
 
     return dataset
 
-# data = get_dataset()
-
-# count = 0
-# for i in list(data.values()):
-#     for q in list(i.values()):
-#         for j in list(q.values()):
-#             print(j)
-#             count+=len(j)
-            
-
-# print(count)
-# print(get_dataset())
-
-# Вивести стовпчикову діаграму: по ОХ на кожну групу дохідності по три стовпчики які відповідають регіонам,
-# по ОУ кількість сімей з таким доходом.
-dataset = get_dataset(22000)
-# income_groups = set(dataset.keys())
-
-inc_regions = {'$0 to <$10,000':{},'$10,000-<$20,000':{},'$20,000-<$30,000':{},'$30,000-<$40,000':{},'$40,000-<$50,000':{},'$50,000+':{}}
-for i in set(dataset.keys()):
-    inc_regions[i] = dict((k,sum(len(dataset[i][k][q]) for q in list(dataset[i][k].keys()))) for k in list(dataset[i].keys())[:3])
-
-# num_of_house_holds = {}
-# for k,v in inc_regions:
-    
-
-print(inc_regions)
-
-
-import plotly.plotly as py
-import plotly.graph_objs as go
-
-
-x=list(inc_regions.keys())
-# print(x,'\n\n',
-#     [list(i.values())[0] for i in list(inc_regions.values())],'\n\n',
-#     [list(list(inc_regions.values())[0].keys())]
-# )
-
-trace0 = go.Bar(
-    x=x,
-    y=[list(i.values())[0] for i in list(inc_regions.values())],
-    text=[list(i.keys())[0] for i in list(inc_regions.values())],
-    textposition='auto',
-    marker=dict(
-        color='rgb(49,130,189)'
-    )
-)
-trace1 = go.Bar(
-    x=x,
-    y=[list(i.values())[1] for i in list(inc_regions.values())],
-    text=[list(i.keys())[1] for i in list(inc_regions.values())],
-    textposition='auto',
-    marker=dict(
-        color='rgb(204,204,204)',
-    )
-)
-trace2 = go.Bar(
-    x=x,
-    y=[list(i.values())[2] for i in list(inc_regions.values())],
-    text=[list(i.keys())[2] for i in list(inc_regions.values())],
-    textposition='auto',
-    marker=dict(
-        color='rgb(158, 64, 147)',
-    )
-)
-
-
-data = [trace0, trace1,trace2]
-layout = go.Layout(
-    xaxis=dict(tickangle=-45),
-    barmode='group',
-)
-
-# fig = go.Figure(data=data, layout=layout)
-pl.plot(data, filename='reg_incgroups_number.html')
 
